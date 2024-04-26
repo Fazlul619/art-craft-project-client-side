8d3d99d7add4f698b4104a261b599251363f49ac
Fazlul Alam <fazlulalam619@gmail.com>
2024-04-26 22:58:59 +0600

    Login Page Designe

--- src/pages/shared/Login Page/Login.jsx
+++ src/pages/shared/Login Page/Login.jsx
@@ -1,5 +1,69 @@
+import { FaGitlab } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+
 const Login = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <div className="hero min-h-screen bg-base-200">
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body">
+              <p className=" text-center font-bold">Please Login</p>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </form>
+            <p className="card-body text-center">
+              Do not Have An Account?
+              <Link className="font-bold text-blue-900" to="/register">
+                Register
+              </Link>
+            </p>
+            <div className="card-body text-2xl items-center ">
+              <p>
+                <button className="mx-5">
+                  <FcGoogle />
+                </button>
+                <button>
+                  <FaGitlab />
+                </button>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
