8aabba5163030a8a16afb2f11abd96aa995d213d
Fazlul Alam <fazlulalam619@gmail.com>
2024-04-30 02:35:48 +0600

    Banner and footer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import { useLoaderData } from "react-router-dom";
 import CraftCard from "./CraftCard";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
   const craftItems = useLoaderData();
   return (
     <div>
+      <Banner></Banner>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-x-4 gap-y-8 w-[90vw] mx-auto mt-8">
         {craftItems.map((craftItem) => (
           <CraftCard key={craftItem._id} craftItem={craftItem}></CraftCard>
